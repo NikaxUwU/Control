e2e29f0af960f1143a93127e6bae109ae30db249
Nika <krasnikova16071607@gmail.com>
2022-12-13 21:46:04 +0300

    Размер будущего массива

--- Работа/Program.cs
+++ Работа/Program.cs
@@ -5,5 +5,19 @@
     return ar;
 }
 
+int Size(string[] MyArray, int n) 
+{
+    int num = 0;
+
+    for(int i = 0; i < MyArray.Length; i++) 
+    {
+        if(MyArray[i].Length <= n) num++;
+    }
+
+    return num;
+}
+
+
+
 string[] MyArray = ReadArray();
 Console.WriteLine($"[{string.Join(" ", MyArray)}]");
