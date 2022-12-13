077b8588277626dfe5b7973b827b5538c52ae6ae
Nika <krasnikova16071607@gmail.com>
2022-12-13 21:42:04 +0300

    Создание массива

---
+++ Работа/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] ReadArray() 
+{
+    Console.Write("Создайте массив (через пробел): ");
+    string [] ar = Console.ReadLine().Split(" ");
+    return ar;
+}
+
+string[] MyArray = ReadArray();
+Console.WriteLine($"[{string.Join(" ", MyArray)}]");
